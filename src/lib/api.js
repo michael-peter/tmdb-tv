be2c635067eaf77656e1833ac41e34fbe68b2a22
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-10 13:05:44 +0100

    fix: used env for secrets

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,7 +4,7 @@ export const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
     'Content-Type': 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZjEzYTBmNWZjOGIzYTQ1NTQzMzU0MjQ1MjIyYzAxZCIsInN1YiI6IjYzZTUxODk5MGU1OTdiMDA3Y2EwOGEwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QMi-7Na0zYFk7g8FHkltuaCFIZHMw1QD-101lmrDu_E',
+    // eslint-disable-next-line no-undef
+    Authorization: `Bearer ${process.env.API_READ_ACCESS_TOKEN}`,
   },
 })
